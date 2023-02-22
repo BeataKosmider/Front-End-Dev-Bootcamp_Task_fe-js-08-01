d4188042a85fd73b0ed37611259d2a32401a95af
Beata Kosmider <kosmibe@gmail.com>
2023-02-22 09:54:42 +0100

    task is done

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 class Apple {
+  constructor(name, color, sweetness) {
+    this.name = "antonowka";
+    this.color = "red";
+    this.sweetness = 5;
+  }
   // tutaj wpisz kod :)
 }
+/*class Human {
+  constructor(name) {
+    this.name = name;
+  }
+}
 
+const human = new Human(‘Tomasz’);*/
 module.exports = {
   Apple,
 };
